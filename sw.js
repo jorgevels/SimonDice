37ed1d73410cac131ae45396b06012dd003b0702
jorgevel <jorgevelasquez006@hotmail.com>
2019-06-14 12:30:50 -0500

    Efectuando pruebas con el SW

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open("video-store").then(function(cache) {
       return cache.addAll([
-        "/SimonDice-/",
-        "/SimonDice-/index.html",
-        "/SimonDice-/js/main.js",
-        "/SimonDice-/index.js",
-        "/SimonDice-/css/style.css"
+        "/SimonDice/",
+        "/SimonDice/index.html",
+        "/SimonDice/index.js",
+        "/SimonDice/css/style.css",
+        "/SimonDice/js/main.js"
       ]);
     })
   );
